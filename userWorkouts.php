1630c4461fc9a0b0bc05c9146fa096b1dfe22a19
Dustin Bingham <djbingham88@gmail.com>
2020-11-29 14:37:58 -0600

    possible final commit before merge

--- userWorkouts.php
+++ userWorkouts.php
@@ -13,9 +13,11 @@
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             
-            echo "Select a Date to Show Workouts completed";
+            
             echo "<form action =\"http://students.cs.niu.edu/~z1756030/workoutdata.php\" method=\"GET\">";
-            //echo "ID: <input type=\"number\" name=\"ID\" min=\"1\"/></br>";
+            echo "<h3>Please select user</h3>";
+            echo "ID: <input type=\"number\" name=\"ID\" min=\"1\"/></br>";
+            echo "<h3>Please select a Date range to show workouts completed</h3>";
             echo "TO: <input type=\"date\" name=\"TO\" />";
             echo "FROM: <input type=\"date\" name=\"FROM\" />";
             echo "<input type=\"submit\" value=\"Selection\" />";
