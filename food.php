23073fe0e26db889c861a4a0bb3212274972f0ab
Reinaldo Moraga <z1864815@hopper.cs.niu.edu>
2020-11-29 20:18:01 -0600

    food.php updated and consumption tracker implemented

--- food.php
+++ food.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-    <title>Food DB</title>
+    <title>Food Tables</title>
 </head>
-<body>
-    <h1>Food</h1>
+<body> 
+    <h1>Food Info</h1>
     <b>Click below for a list of all foods!</b> <br/><br/>
     <a href="?listF=all">List all Foods</a>
     <br/><br/>
@@ -13,8 +13,34 @@
 
 	<input type="submit" value="Go!"/>
     </form>
+    <h2>Track Consumption</h2>
+    <b>Fill in the information below to compare your nutrient consumption!</b> <br></br>
+    <form action="micro.php" method="POST">
+    Select a micronutrient: <select name="micro" id="micro">
+    	<option value="Vitamin A">Vitamin A</option>
+    	<option value="Vitamin C">Vitamin C</option>
+    	<option value="Vitamin D">Vitamin D</option>
+    	<option value="Vitamin E">Vitamin E</option>
+    	<option value="Calcium">Calcium</option>
+    	<option value="Chloride">Chloride</option>
+    	<option value="Magnesium">Magnesium</option>
+    	<option value="Phosphorus">Phosphorus</option>
+    	<option value="Potassium">Potassium</option>
+    	<option value="Sodium">Sodium</option>
+	<option value="Sulfur">Sulfur</option>
+     </select>
+	&nbsp;&nbsp;Your Intake: <input size=5 type="text" name="amount"/>
+     <select name="units" id="units">
+	<option value="g">g</option>
+	<option value="mg">mg</option>
+	<option value="mcg">mcg</option>
+     </select>
+	&nbsp;&nbsp;In how many days? <input size=5 type="text" name="days"/> days
+     <br><br>
+     <input type="submit" value="Submit"/>
+     </form>
 <?php
-include('userpass.php');
+include('secrets.php');
 include('DrawTable.php');
 
 	try {
